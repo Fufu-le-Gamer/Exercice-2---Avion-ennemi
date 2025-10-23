7d9eb398fae40a4312e7b9b9a07802f0952dbaae
Fufu-le-Gamer <yducrocq@gaming.tech>
2025-10-23 15:45:47 +0200

    Ajoutez des fichiers projet.

---
+++ Exercice 2 - Avion ennemi/Exercice 2 - Avion ennemi.cpp
@@ -0,0 +1,26 @@
+
+
+#include <iostream>
+
+int main()
+{
+	float a;
+	float b;
+	float c;
+
+	std::cout << "Valeur de a: ";
+	std::cin >> a;
+	std::cout << "Valeur de b: ";
+	std::cin >> b;
+	std::cout << "Valeur de c: ";
+	std::cin >> c;
+	if (a * 10 * 10 + b * 10 + c > 0) {
+		std::cout << "L'avion ennemi passe au dessus";
+	}
+	else if (a * 10 * 10 + b * 10 + c < 0) {
+		std::cout << "L'avion ennemi passe en dessous";
+	}
+	else {
+		std::cout << "L'avion entre en collision avec l'IA";
+	}
+}
