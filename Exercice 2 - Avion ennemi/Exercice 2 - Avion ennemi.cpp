e85f82698bae603892e736eaaaf0e59824783217
Fufu-le-Gamer <yducrocq@gaming.tech>
2025-10-23 15:50:57 +0200

    corriger

--- Exercice 2 - Avion ennemi/Exercice 2 - Avion ennemi.cpp
+++ Exercice 2 - Avion ennemi/Exercice 2 - Avion ennemi.cpp
@@ -7,20 +7,26 @@ int main()
 	float a;
 	float b;
 	float c;
+	bool reponse = true;
 
-	std::cout << "Valeur de a: ";
-	std::cin >> a;
-	std::cout << "Valeur de b: ";
-	std::cin >> b;
-	std::cout << "Valeur de c: ";
-	std::cin >> c;
-	if (a * 10 * 10 + b * 10 + c > 0) {
-		std::cout << "L'avion ennemi passe au dessus";
-	}
-	else if (a * 10 * 10 + b * 10 + c < 0) {
-		std::cout << "L'avion ennemi passe en dessous";
-	}
-	else {
-		std::cout << "L'avion entre en collision avec l'IA";
+	while (reponse) {
+		std::cout << "Valeur de a: ";
+		std::cin >> a;
+		std::cout << "Valeur de b: ";
+		std::cin >> b;
+		std::cout << "Valeur de c: ";
+		std::cin >> c;
+		if (a * 10 * 10 + b * 10 + c > 0) {
+			std::cout << "L'avion ennemi passe au dessus";
+		}
+		else if (a * 10 * 10 + b * 10 + c < 0) {
+			std::cout << "L'avion ennemi passe en dessous";
+		}
+		else {
+			std::cout << "L'avion entre en collision avec l'IA";
+		}
+		std::cout << "\n";
+		std::cout << "Rejouez ?";
+		std::cin >> reponse;
 	}
 }
